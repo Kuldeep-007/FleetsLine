8b6e4039b1ae4277749057fb2ec38a6ba8c72ff4
kuldeeps <kuldeep.singh@ellucian.com>
2020-08-27 12:16:01 +0530

    Adding mock data by an XML

---
+++ Fleets.Line/Fleets.Line/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fleets.Line.Controllers
+{
+    /// <summary>
+    /// Home controller
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Land on home page and choose how to proceed further:
+        /// => As a Fleet Manager for FleetsLine
+        /// => Or as a Customer on FleetsLine portal
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
